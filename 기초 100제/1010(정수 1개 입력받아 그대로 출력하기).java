f2e5337d0e89cf48406f54717ace30ea3fd356eb
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-07 17:16:52 +0900

    Create 1010(정수 1개 입력받아 그대로 출력하기).java

---
+++ 기초 100제/1010(정수 1개 입력받아 그대로 출력하기).java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        System.out.print(a);
+    }
+}
