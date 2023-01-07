fced6d64b8aad505739f0b63eb0759ea5b3300c5
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-07 17:09:42 +0900

    Create 1008(출력하기08).java

---
+++ 기초 100제/1008(출력하기08).java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("\u250C\u252C\u2510\n");
+        System.out.print("\u251C\u253C\u2524\n");
+        System.out.print("\u2514\u2534\u2518");
+    }
+}
