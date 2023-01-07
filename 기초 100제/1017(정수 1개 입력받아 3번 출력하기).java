37c7c9f53d3edd589eccfacb5e27c909503182e8
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-07 18:03:58 +0900

    Create 1017(정수 1개 입력받아 3번 출력하기).java

---
+++ 기초 100제/1017(정수 1개 입력받아 3번 출력하기).java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int a = Integer.parseInt(br.readLine());
+        System.out.print(a + " " + a+ " " + a);
+    }
+}
