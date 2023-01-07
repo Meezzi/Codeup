08f23821c4b0465f68e808a00a482475db9d0695
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-07 17:59:17 +0900

    Create 1014(문자 2개 입력받아 순서 바꿔 출력하기).java

---
+++ 기초 100제/1014(문자 2개 입력받아 순서 바꿔 출력하기).java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String a = st.nextToken();
+        String b = st.nextToken();
+
+        System.out.println(b + " " + a);
+    }
+}
