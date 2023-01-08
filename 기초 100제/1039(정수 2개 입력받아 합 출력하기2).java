40f6d4c4aa97805b0d73b96646dac42f876ba634
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-08 21:31:23 +0900

    Create 1039(정수 2개 입력받아 합 출력하기2).java

---
+++ 기초 100제/1039(정수 2개 입력받아 합 출력하기2).java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+
+        System.out.print(a.add(b));
+    }
+}
