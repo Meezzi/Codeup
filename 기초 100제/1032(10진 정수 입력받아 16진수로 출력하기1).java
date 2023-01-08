813a834728c1f9af077e7249290dad3d43bbc531
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-08 20:57:58 +0900

    Create 1032(10진 정수 입력받아 16진수로 출력하기1).java

---
+++ 기초 100제/1032(10진 정수 입력받아 16진수로 출력하기1).java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+
+        System.out.format("%x", a);
+    }
+}
