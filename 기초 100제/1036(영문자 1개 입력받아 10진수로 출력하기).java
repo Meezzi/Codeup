6b488f5d79e37e4abdee5d0caf8c1f1fc724c1f6
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-08 21:23:19 +0900

    Create 1036(영문자 1개 입력받아 10진수로 출력하기).java

---
+++ 기초 100제/1036(영문자 1개 입력받아 10진수로 출력하기).java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int a = s.charAt(0);
+        System.out.print(a);
+    }
+}
