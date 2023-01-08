503fe04250fb21f0a100287ee2081bf8e3b67d89
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-08 21:48:48 +0900

    Create 1041(문자 1개 입력받아 다음 문자 출력하기).java

---
+++ 기초 100제/1041(문자 1개 입력받아 다음 문자 출력하기).java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        char a = (char) (s.charAt(0)+1);
+        System.out.print(a);
+    }
+}
