40bbcc550078c8f46114e9cd7e3b6b0d33577337
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-09 10:51:42 +0900

    Create 1063(두 정수 입력받아 큰 수 출력하기).java

---
+++ 기초 100제/1063(두 정수 입력받아 큰 수 출력하기).java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.print(a>b ? a: b);
+    }
+}
