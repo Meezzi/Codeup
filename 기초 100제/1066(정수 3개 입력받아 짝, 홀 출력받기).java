1377172c36c654a8231e3da8365f3702562efb8e
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-09 11:14:32 +0900

    Create 1066(정수 3개 입력받아 짝, 홀 출력받기).java

---
+++ 기초 100제/1066(정수 3개 입력받아 짝, 홀 출력받기).java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if(a%2==0) System.out.println("even");
+        else System.out.println("odd");
+        if(b%2==0) System.out.println("even");
+        else System.out.println("odd");
+        if(c%2==0) System.out.println("even");
+        else System.out.println("odd");
+    }
+}
