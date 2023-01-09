4de8c85db5c332e32dd5d54bb2913e9156a3da20
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-09 09:35:40 +0900

    Create 1052(두 정수 입력받아 비교하기4).java

---
+++ 기초 100제/1052(두 정수 입력받아 비교하기4).java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if(a!=b) System.out.println(1);
+        else System.out.print(0);
+    }
+}
