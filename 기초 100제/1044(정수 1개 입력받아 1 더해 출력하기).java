439e7177389d10224e2c9e4bace6e7d34d0db158
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-09 09:11:47 +0900

    Create 1044(정수 1개 입력받아 1 더해 출력하기).java

---
+++ 기초 100제/1044(정수 1개 입력받아 1 더해 출력하기).java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long a = Long.parseLong(br.readLine());
+
+        System.out.print(a+1);
+    }
+}
