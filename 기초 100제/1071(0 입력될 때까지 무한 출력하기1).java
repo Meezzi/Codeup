ffa10f87c24733c0ce631df1340f905cde3a84f1
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-09 11:43:02 +0900

    Create 1071(0 입력될 때까지 무한 출력하기1).java

---
+++ 기초 100제/1071(0 입력될 때까지 무한 출력하기1).java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+
+        while(a!=0) {
+            System.out.println(a + " ");
+            a = Integer.parseInt(st.nextToken());
+
+        }
+    }
+}
