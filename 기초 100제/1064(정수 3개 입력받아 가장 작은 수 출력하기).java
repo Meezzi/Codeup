29f1a28d59d82ad2a69e41ffd7274ab4f467cd1a
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-09 10:57:18 +0900

    Create 1064(정수 3개 입력받아 가장 작은 수 출력하기).java

---
+++ 기초 100제/1064(정수 3개 입력받아 가장 작은 수 출력하기).java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.print((a>b ? b: a)>c ? c : a>b ? b: a);
+    }
+}
