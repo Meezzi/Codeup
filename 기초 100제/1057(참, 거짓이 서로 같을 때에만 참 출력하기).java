779680d44111482c1335b2a5c4e54fa7fc040711
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-09 10:08:53 +0900

    Create 1057(참, 거짓이 서로 같을 때에만 참 출력하기).java

---
+++ 기초 100제/1057(참, 거짓이 서로 같을 때에만 참 출력하기).java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        boolean c = (a==b);
+        
+        if (c) System.out.print(1);
+        else System.out.print(0);
+    }
+}
