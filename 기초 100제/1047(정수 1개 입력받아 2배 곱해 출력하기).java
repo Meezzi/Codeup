69475aa553c525c22ab71d611956a0f88f686c45
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-09 09:25:50 +0900

    Create 1047(정수 1개 입력받아 2배 곱해 출력하기).java

---
+++ 기초 100제/1047(정수 1개 입력받아 2배 곱해 출력하기).java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+
+        System.out.println(a<<1);
+    }
+}
