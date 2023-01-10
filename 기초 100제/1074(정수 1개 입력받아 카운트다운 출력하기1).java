2be0d17ecca4945f626b26d19d598caa565bbdf6
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-10 10:10:28 +0900

    Create 1074(정수 1개 입력받아 카운트다운 출력하기1).java

---
+++ 기초 100제/1074(정수 1개 입력받아 카운트다운 출력하기1).java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+
+        for(int i=a; i>0; i--) {
+            System.out.println(i);
+        }
+    }
+}
