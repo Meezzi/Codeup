e6a0d352ec3c6e48405a2705568aeb0d1d9a96c3
강민지 <68285922+Kminzzi@users.noreply.github.com>
2023-01-10 13:20:19 +0900

    Create 1088(3의 배수는 통과?).java

---
+++ 기초 100제/1088(3의 배수는 통과?).java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=a; i++) {
+            if(i%3==0) continue;
+            System.out.print(i+" ");
+        }
+    }
+}
